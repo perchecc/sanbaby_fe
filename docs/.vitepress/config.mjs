1a16d30db5ee472d1b96b95547c9cabeaf378eae
perchecc <perchecc@163.com>
2024-01-24 22:14:47 +0800

    feat:添加vite-plugin-vitepress-auto-sidebar插件，方便自动生成文件夹下的文件目录

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "vitepress";
+import AutoSidebar from "vite-plugin-vitepress-auto-sidebar";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  vite: {
+    plugins: [AutoSidebar({ prefix: ".", collapsed: false })],
+  },
   title: "前端加油站",
   description: "vitepress搭建的博客，前端加油站，学习前端知识",
   themeConfig: {
@@ -9,31 +13,22 @@ export default defineConfig({
     logo: "/logo.png",
     siteTitle: false,
     head: [["link", { rel: "icon", type: "image/ico", href: "/favicon.ico" }]],
+    lastUpdatedText: "更新时间",
+    outline: "deep",
+    outlineTitle: "大纲",
+    footer: {
+      message: "sanbaby - 前端的加油站",
+      copyright: "Copyright © 2019-present 随易科技",
+    },
+    docFooter: {
+      prev: "上一页",
+      next: "下一页",
+    },
     nav: [
       { text: "Home", link: "/" },
       { text: "吃", link: "/eat/index" },
       { text: "工作复盘", link: "/工作复盘/工作每日复盘2024-01-03" },
-      { text: "Examples", link: "/markdown-examples" },
-      { text: "Examples", link: "/markdown-examples" },
     ],
-
-    sidebar: [
-      {
-        text: "吃",
-        items: [
-          { text: "青岛小花蛤", link: "/eat/青岛小花蛤" },
-          { text: "Runtime API Examples", link: "/api-examples" },
-        ],
-      },
-      {
-        text: "工作复盘",
-        items: [
-          { text: "工作每日复盘2024-01-03", link: "/工作复盘/工作每日复盘2024-01-03" },
-          { text: "Runtime API Examples", link: "/api-examples" },
-        ],
-      },
-    ],
-
     socialLinks: [{ icon: "github", link: "https://github.com/perchecc" }],
   },
 });
